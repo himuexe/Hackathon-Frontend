65044b02aa4250bcabdc0d02bf25b4ee04e95bf2
Himanshu Sharma <sharmaslov@gmail.com>
2024-07-26 21:45:03 +0530

    Navbar

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css'
+import Navbar from './components/navbar/navbar';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+    <Route
+          path="/"
+          className="bg"
+          element={
+            <div>
+              <Navbar />
+              {/* <Hero /> */}
+            </div>
+          }
+        />
+    </Routes>
+  </BrowserRouter>
+  </React.StrictMode>,
+)
